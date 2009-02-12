393e9c856aceba712624aa9b7d8c7e74226417e9
scorlosquet <scorlosquet@9bc17e61-e048-0410-9b95-ddf33fbbb69f>
2009-02-12 18:14:44 +0000

    improving indentation

--- branches/DRUPAL-6--2/sites/all/modules/neologism/js/neologism.js
+++ branches/DRUPAL-6--2/sites/all/modules/neologism/js/neologism.js
@@ -1,9 +1,11 @@
 if (Drupal.jsEnabled) {
-  $(document).ready(function neo_autofill() {
-    $('#edit-title').bind('blur', function() {
-      if ($('#edit-field-label-0-value').val() == '') {
-        $('#edit-field-label-0-value').val(this.value);
-      }
-    });
-  });
+  $(document).ready(
+    function neo_autofill() {
+      $('#edit-title').bind('blur', function() {
+        if ($('#edit-field-label-0-value').val() == '') {
+          $('#edit-field-label-0-value').val(this.value);
+        }
+      });
+    }
+  );
 }
