1cf2bbdc5cac08802bfc9dd079206e7b0998ce33
scorlosquet <scorlosquet@9bc17e61-e048-0410-9b95-ddf33fbbb69f>
2009-03-01 21:25:29 +0000

    add RDF icon for export on node

--- branches/DRUPAL-6--2/themes/garland/node.tpl.php
+++ branches/DRUPAL-6--2/themes/garland/node.tpl.php
@@ -28,5 +28,6 @@
       <div class="links"><?php print $links; ?></div>
     <?php endif; ?>
   </div>
+<p><a href="<?php print $node_url ?>/rdf"><img alt="RDF export" title="RDF export" src="http://www.w3.org/RDF/icons/rdf_w3c_button.32" /></a></p>
 
 </div>
