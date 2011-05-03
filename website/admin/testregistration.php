56f12d117f16afcd9137783822705d88885d23b5
guidocecilio <guidocecilio@gmail.com>
2011-05-03 10:01:31 +0000

    Added a test option to check when is or is not the webservice working.

--- website/admin/testregistration.php
+++ website/admin/testregistration.php
@@ -1,7 +1,7 @@
 <?php
 $ch = curl_init();
 
-curl_setopt($ch, CURLOPT_URL, "http://localhost/neologism-website/admin/registration.php");
+curl_setopt($ch, CURLOPT_URL, "http://neologism.deri.ie/admin/registration.php");
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, "customer_name=guido&organization=DERI&email=".urlencode('guido.cecilio@deri.org')."&website_uri=".urlencode('http://vocab.deri.ie')."&plan=".urlencode('this is a test'));
 
